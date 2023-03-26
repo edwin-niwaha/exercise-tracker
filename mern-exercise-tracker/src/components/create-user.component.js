973c9a96d33315c84b5db2e022cdac078822aede
Edwin <ed107107@gmail.com>
2023-03-26 23:21:00 +0300

    Add components

---
+++ mern-exercise-tracker/src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreateUser extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are welcome CreateUser</p>
+      </div>
+    );
+  }
+}
